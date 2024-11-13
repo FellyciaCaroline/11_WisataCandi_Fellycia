c96fdd849212823f9c8383c75e4344852d804f83
FellyciaCaroline <165134673+FellyciaCaroline@users.noreply.github.com>
2024-11-13 10:58:30 +0700

    Pertemuan 15

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
 import 'package:wisata_candi/screens/sign_in_screen.dart';
 import 'package:wisata_candi/screens/sign_up_screen.dart';
 import 'screens/detail_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/sign_in_screen.dart';
+import 'screens/search_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,27 +18,27 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Candi',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.deepPurple),
-            titleTextStyle: TextStyle(
-              color: Colors.deepPurple,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            )),
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
-          primary: Colors.deepPurple,
-          surface: Colors.deepPurple[50],
+        title: 'Wisata Candi',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              iconTheme: IconThemeData(color: Colors.deepPurple),
+              titleTextStyle: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+            primary: Colors.deepPurple,
+            surface: Colors.deepPurple[50],
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
-      ),
-      home:
-          // DetailScreen(candi: candiList[0]),
-          // const ProfileScreen()
-          // SignInScreen(),
-          SignUpScreen(),
-    );
+        home:
+            // DetailScreen(candi: candiList[0]),
+            // const ProfileScreen()
+            // SignInScreen(),
+            // SignUpScreen(),
+            SeacrhScreen());
   }
 }
